92aedacdbbd5f4afd1f9f92ba36ccb3f68b5d912
duartesluiza <duartes.luiza92@gmail.com>
2023-06-11 20:57:20 -0300

    Commit inicial do projeto

---
+++ screens/Login.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Button, Input, Text } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import styles from '../style/MainStyle';
+
+
+export default function Login({ navigation }) {
+
+    const [email, setEmail] = useState(null)
+    const [password, setPassword] = useState(null)
+
+    const entrar = () => {
+        navigation.reset({
+            index: 0,
+            routes: [{ name: "Principal" }]
+        })
+    }
+
+    return (
+        <View styles={[styles.container, specificStyle.specificContainer]}>
+            <Text h3>Entre no AppFridas</Text>
+            <Input
+                placeholder="E-mail"
+                leftIcon={{ type: 'font-awesome', name: 'envelope' }}
+                onChangeText={value => setEmail(value)}
+                keyboardType="email-address"
+            />
+            <Input
+                placeholder="Sua senha"
+                leftIcon={{ type: 'font-awesome', name: 'lock' }}
+                onChangeText={value => setPassword(value)}
+                secureTextEntry={true}
+            />
+            <Button
+                icon={
+                    <Icon
+                        name="check"
+                        size={15}
+                        color="white"
+                    />
+                }
+                title="Entrar"
+                /*Evento do click     */
+                onPress={() => entrar()}
+
+            />
+
+
+        </View>
+    );
+}
+
+
+const specificStyle = StyleSheet.create({
+    specificContainer: {
+        backgroundColor: "#fff"
+    }
+})
